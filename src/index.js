8e64e6a36904ba722f668156606a9e93d3fedc4d
Edw1x <erqwrewre12@gmail.com>
2020-03-15 18:37:50 +0200

    deploy my react app to github pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./routes";
+import App from "./App.js"
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<Routes />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
