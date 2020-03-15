e087ea2fb7917e1aebd2ad6d761e49b59ec37d0a
Edw1x <erqwrewre12@gmail.com>
2020-03-15 18:42:02 +0200

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import Page1 from "./routes/Page1";
+import Page2 from "./routes/Page2";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <h2>you beach</h2>
-      <a src="https://images.pexels.com/photos/2882/man-person-street-shoes.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500">
-        hi pls
-      </a>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Page1} />
+          <Route exact path="/page2" component={Page2} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
