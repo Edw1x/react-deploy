e5d552cf36cc8b38921e775edf501611dcc8d798
Edw1x <erqwrewre12@gmail.com>
2020-03-15 18:28:58 +0200

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
     <div className="App">
       <h1>Hello</h1>
       <h2>you beach</h2>
+      <a src="https://images.pexels.com/photos/2882/man-person-street-shoes.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500">
+        hi pls
+      </a>
     </div>
   );
 }
