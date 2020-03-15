74090abdfe79d5d1d7672d0ceba7b3958ed8219b
Edw1x <erqwrewre12@gmail.com>
2020-03-15 21:05:41 +0200

    Updates

---
+++ precache-manifest.4f39cf9299a116daf1131bc2058a563e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be4cbd72857edaf1fd626be703c8f165",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "a6179f33d3c0c61a4832",
+    "url": "/react-deploy/static/css/main.4980d82e.chunk.css"
+  },
+  {
+    "revision": "dd1929099ebe63a5c173",
+    "url": "/react-deploy/static/js/2.0647427d.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/react-deploy/static/js/2.0647427d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6179f33d3c0c61a4832",
+    "url": "/react-deploy/static/js/main.419701d5.chunk.js"
+  },
+  {
+    "revision": "fc04b6ae18b669ce4a52",
+    "url": "/react-deploy/static/js/runtime-main.66bdc372.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-deploy/precache-manifest.32c9c25add7d7b71a99b727ce0bcbd45.js"
+  "/react-deploy/precache-manifest.4f39cf9299a116daf1131bc2058a563e.js"
 );
 
 self.addEventListener('message', (event) => {
