f8a06542c93c520a10d004509d476383b024f296
Edw1x <erqwrewre12@gmail.com>
2020-03-15 18:19:24 +0200

    Updates

---
+++ precache-manifest.d6cca4bc44cb979572cb5fe35be168b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1925ae0e9e7bfc882c595235c61697de",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "61096f003d58dac2c2d3",
+    "url": "/react-deploy/static/css/main.7928b2f3.chunk.css"
+  },
+  {
+    "revision": "293962de1d7888680d39",
+    "url": "/react-deploy/static/js/2.dbf30e0a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-deploy/static/js/2.dbf30e0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61096f003d58dac2c2d3",
+    "url": "/react-deploy/static/js/main.fe6d798f.chunk.js"
+  },
+  {
+    "revision": "fc04b6ae18b669ce4a52",
+    "url": "/react-deploy/static/js/runtime-main.66bdc372.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
